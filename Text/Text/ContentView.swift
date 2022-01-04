e85aa8e95c9894a497817acd0038b54c9aae6b04
pulinghao <pulinghaox@163.com>
2022-01-04 23:03:47 +0800

    提交text

--- Text/Text/ContentView.swift
+++ Text/Text/ContentView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
+    let alignments : [TextAlignment] = [.leading, .center, .trailing]
+    @State private var alignment = TextAlignment.leading
     var body: some View {
         VStack{
             Text("Hello, world!")
@@ -15,6 +17,22 @@ struct ContentView: View {
             Text("This is some longer text that is limited to three lines maximum, so anything more than that will cause the text to clip.")
                 .lineLimit(3)
                 .frame(width: 200)
+                .font(.largeTitle)
+            Text("The best laid plans")
+                .padding()
+                .background(Color.yellow)
+                .foregroundColor(.white)
+                .font(.headline)
+            
+            Picker("text alignment", selection: $alignment){
+                ForEach(alignments, id:\.self){ alignment in
+                    Text(String(describing: alignment))
+                }
+            }
+            Text("This is an extremely long text string that will never fit even the widest of phones without wrapping")
+                    .font(.largeTitle)
+                    .multilineTextAlignment(alignment)
+                    .frame(width: 300)
         }
         
     }
